b45824b44933550eaf9e06ff400a0e105784214b
jingyuanling <2937498097@qq.com>
2019-04-20 10:13:30 +0800

    wine

---
+++ winePC/src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home_content">
+    <home-banner></home-banner>
+  </div>
+</template>
+
+<script>
+import HomeBanner from './components/Banner'
+export default {
+  name: 'Home',
+  components: {
+    HomeBanner
+  }
+}
+</script>
+
+<style>
+  .home_content
+    width:1200px
+</style>
